e03c04b75e03450b799ff0d3832dae740f8734a0
Adhikkesh <adhikkesh@gmail.com>
2026-02-07 19:46:15 +0530

    chore: add shared constant

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,10 +1,1 @@
-export const RIDE_STATUS = {
-  COMPLETED: "COMPLETED",
-  MATCHED: "MATCHED",
-  SEARCHING: "SEARCHING",
-} as const;
-
-export interface User {
-  uid: string;
-  role: "rider" | "driver";
-}
+export const SHARED_CONSTANT = "shared";
