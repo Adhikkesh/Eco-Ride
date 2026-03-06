b6f21eb5a712a0f788678e7ba86723280b7ebb55
Nitin-R-S <nitincool1013@gmail.com>
2026-03-06 11:09:59 +0530

    feat: Register `url_launcher_web` plugin for mobile and remove the backend simulator's `dev` script.

--- apps/mobile/.dart_tool/dartpad/web_plugin_registrant.dart
+++ apps/mobile/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -15,6 +15,7 @@ import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:location_web/location_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
@@ -28,5 +29,6 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   GoogleMapsPlugin.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
